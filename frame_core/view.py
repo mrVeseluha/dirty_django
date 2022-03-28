04e69bc37a7fb196ca747b0c52476fa04c1aa11c
mrveseluh <maxim@mrveseluh.pub>
2022-03-28 13:52:20 +0300

    Переделан класс отображающий таблица датафреймов Pandas

--- frame_core/view.py
+++ frame_core/view.py
@@ -44,8 +44,36 @@ class IndexView(BaseView):
 
 class PandasTableView(BaseView):
     df = None
+    filter = None
 
     def get_context(self):
         context = super(PandasTableView, self).get_context()
-        context['data_frame'] = self.df
+        df = self.df
+
+        if self.request.get('GET'):
+            sort_field = self.request['GET'].get('sort')
+            if sort_field and ('-' == sort_field[0]):
+                sort_field = sort_field[1:]
+                ascending = False
+            else:
+                ascending = True
+            if sort_field and (sort_field in df.columns):
+                df.sort_values(by=sort_field, ascending=ascending, inplace=True)
+
+            filter = self.request['GET'].get('filter')
+            if filter and (filter in df.columns):
+                context['filter'] = {'name': filter,
+                                     'values': df[filter].unique()}
+                for key, val in self.request['GET'].items():
+                    if (key in df.columns) and val:
+                        try:
+                            val = int(val)
+                        except:
+                            try:
+                                val = float(val)
+                            except:
+                                pass
+                        df = df[df[key] == val]
+
+        context['data_frame'] = df
         return context
