b15868527dfda2444870cd87abcb21b5f5d95e81
mrveseluh <maxim@mrveseluh.pub>
2022-03-28 13:53:10 +0300

    Переименнован чтобы соответствовать значению паттерна

--- frame_core/front_controller.py
+++ frame_core/front_controller.py
@@ -1,4 +1,5 @@
 import datetime
+import urllib.parse
 
 
 def request_datetime(environ):
@@ -17,20 +18,22 @@ def base_middleware(environ):
                 result[k] = v
         return result
 
-    result = dict()
+    result = {'URL': environ['PATH_INFO'],
+              'QUERY_STRING': environ['QUERY_STRING']}
     method = environ['REQUEST_METHOD']
     if method == 'GET':
         value = parse_input_data(environ['QUERY_STRING'])
     if method == 'POST':
         content_length_data = environ.get('CONTENT_LENGTH')
         content_length = int(content_length_data) if content_length_data else 0
-        data = environ['wsgi.input'].read(content_length) if content_length > 0 else b''
-        value = parse_input_data(data.decode(encoding='utf-8'))
+        data = environ['wsgi.input'].read(content_length).decode('utf-8') if content_length > 0 else ''
+        data = urllib.parse.parse_qs(data, keep_blank_values=True)
+        data = dict((key, val if len(val) > 1 else val[0]) for key, val in data.items())
+        value = data
     if method:
         result[method] = value
     result['REQUEST_METHOD'] = method
 
     data = environ.get('CONTENT_LENGTH')
-    print(data)
 
     return result
