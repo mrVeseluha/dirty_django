a99d32905c8f4ad2cd12157e314f48886f49c705
mrveseluh <maxim@mrveseluh.pub>
2022-03-28 14:41:27 +0300

    Полностью переделан парсинг GET и POST запросов. Тепрь используется стандартная библиотека urllib.parse

--- frame_core/front_controller.py
+++ frame_core/front_controller.py
@@ -7,33 +7,24 @@ def request_datetime(environ):
 
 
 def base_middleware(environ):
-    def parse_input_data(data: str):
-        result = {}
-        if data:
-            # делим параметры через &
-            params = data.split('&')
-            for item in params:
-                # делим ключ и значение через =
-                k, v = item.split('=')
-                result[k] = v
-        return result
-
     result = {'URL': environ['PATH_INFO'],
               'QUERY_STRING': environ['QUERY_STRING']}
     method = environ['REQUEST_METHOD']
+
     if method == 'GET':
-        value = parse_input_data(environ['QUERY_STRING'])
+        value = environ['QUERY_STRING']
+        value = dict(urllib.parse.parse_qsl(value))
+
     if method == 'POST':
         content_length_data = environ.get('CONTENT_LENGTH')
         content_length = int(content_length_data) if content_length_data else 0
         data = environ['wsgi.input'].read(content_length).decode('utf-8') if content_length > 0 else ''
-        data = urllib.parse.parse_qs(data, keep_blank_values=True)
-        data = dict((key, val if len(val) > 1 else val[0]) for key, val in data.items())
+        data = dict(urllib.parse.parse_qsl(data, keep_blank_values=True))
         value = data
+
     if method:
         result[method] = value
-    result['REQUEST_METHOD'] = method
 
-    data = environ.get('CONTENT_LENGTH')
+    result['REQUEST_METHOD'] = method
 
     return result
