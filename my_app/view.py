60ab0d4d24d3c85f60f0ea78862d762dee2a21a0
mrveseluh <maxim@mrveseluh.pub>
2022-03-28 13:45:18 +0300

    Переделаны классы отображающие страницы MessageBoard и PandasExampleTable

--- my_app/view.py
+++ my_app/view.py
@@ -1,5 +1,8 @@
 from frame_core.view import BaseView, PandasTableView
 import pandas as pd
+from urllib.parse import unquote
+import pickle
+import os
 
 
 class myView(BaseView):
@@ -11,9 +14,35 @@ class myPandasView(PandasTableView):
         'https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv')
     template = 'pandas_view_example.html'
 
+    def POST(self):
+        csv = self.request['POST'].get('csv')
+        if csv:
+            self.df = pd.read_csv(unquote(unquote(csv)))
+        return self.GET()
+
+
+class myMessageBoardView(BaseView):
+    template = 'message_board.html'
+    model_name = 'message_board.pkl'
+
     def get_context(self):
-        context = super().get_context()
-        if self.request.get('POST'):
-            df = context['data_frame']
-            context['data_frame'] = df[df['variety'] == self.request['POST']['variety']]
+        context = super(myMessageBoardView, self).get_context()
+        if os.path.exists(self.model_name):
+            with open(self.model_name, 'rb') as f:
+                data = pickle.load(f)
+            context['messages'] = data
         return context
+
+    def POST(self):
+        if os.path.exists(self.model_name):
+            with open(self.model_name, 'rb') as f:
+                data = pickle.load(f)
+        else:
+            data = []
+
+        data.append(self.request.get('POST'))
+
+        with open(self.model_name, 'wb') as f:
+            pickle.dump(data, f)
+
+        return self.GET()
